b3ff0cdb779021256091f963d9ae431c9260beb9
Procrastinator24 <126313984+Procrastinator24@users.noreply.github.com>
2025-04-08 20:13:00 +0700

    Integer12

--- Program.cs
+++ Program.cs
@@ -2,13 +2,17 @@
 
 class Program
 {
-    const double PI = 3.14159;
-    const double radius = 5.0;
+  
 
     static void Main()
     {
-        var area = PI*radius*radius;
-        Console.WriteLine($"Area of the circle: {area}");
+        string str = Console.ReadLine();
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            Console.Write(str[i]);
+        }
+
+
 
 
 
