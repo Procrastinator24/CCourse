e26abedc7e94d45831e9584716bbb37ebef72272
Procrastinator24 <126313984+Procrastinator24@users.noreply.github.com>
2025-04-08 19:43:35 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    const double PI = 3.14159;
+    const double radius = 5.0;
+
+    static void Main()
+    {
+        var area = PI*radius*radius;
+        Console.WriteLine($"Area of the circle: {area}");
+
+
+
+    }
+}
